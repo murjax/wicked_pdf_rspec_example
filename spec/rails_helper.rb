2ceff8aa62eccbf2a42f9ec415f250e11c63da37
Ryan Murphy <murjax@gmail.com>
2018-09-22 15:04:25 -0400

    Move pdf text logic to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Capybara::DSL
+
+  def pdf_text(page)
+    temp_pdf = Tempfile.new('pdf')
+    temp_pdf << page.source.force_encoding('UTF-8')
+    reader = PDF::Reader.new(temp_pdf)
+    pdf_text = reader.pages.map(&:text).first
+    filtered_pdf_text = pdf_text.gsub("\n", ' ').gsub("\t", ' ')
+  end
 end
--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -7,13 +7,8 @@ describe 'Documents', type: :request do
       document_body = "Just pretend you are a whisper floating across a mountain. Don't fiddle with it all day. Get tough with it, get strong."
       document = Document.create(name: document_name, body: document_body)
       visit document_path(document)
-      temp_pdf = Tempfile.new('pdf')
-      temp_pdf << page.source.force_encoding('UTF-8')
-      reader = PDF::Reader.new(temp_pdf)
-      pdf_text = reader.pages.map(&:text).first
-      filtered_pdf_text = pdf_text.gsub("\n", ' ').gsub("\t", ' ')
-      expect(filtered_pdf_text).to include(document_name)
-      expect(filtered_pdf_text).to include(document_body)
+      expect(pdf_text(page)).to include(document_name)
+      expect(pdf_text(page)).to include(document_body)
     end
   end
 end
